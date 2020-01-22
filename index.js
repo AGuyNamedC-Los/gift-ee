393fecfc08a1ba7f9867978901527ca2720b00f1
Carlos Castillo <closcastillo95@gmail.com>
2020-01-22 15:13:08 -0800

    added things

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
 const express = require('express');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
-const database = require('nedb');
+const DataStore = require('nedb');
 const bcrypt = require('bcryptjs');
 
 var app = express();
@@ -30,7 +30,7 @@ nunjucks.configure('templates', {
 const template = nunjucks.precompile(
   './templates/base.html',
   { name: 'base' }
-)
+);
 
 const cookieName="yummy_cookie";
 app.use(session({
@@ -38,7 +38,24 @@ app.use(session({
 	resave: false,
 	saveUninitialized: false,
 	name: cookieName
-}))
+}));
+
+const setUpSessionMiddleware = function(req, res, next) {
+	if(!req.session.user) {
+		req.session.user = {role: "guest", firstName: "", lastName: ""};
+	}
+	next();
+};
+app.use(setUpSessionMiddleware);
+
+// this middleware restricts paths to only logged in users
+const loggedInMiddleware = function(req, res, next) {
+	if(req.session.user.role != "user") {
+		res.render("users_only.html");
+	} else {
+		next();
+	}
+};
 
 /* ----------------------------------------------------WEBPAGES---------------------------------------------------- */
 app.get('/', function (req, res) {
@@ -53,12 +70,30 @@ app.get('/sign-up', function (req, res) {
     res.render('sign_up.html');
 });
 
-app.get('/profile', function (req, res) {
+app.get('/profile', loggedInMiddleware, function (req, res) {
     res.render('gift-ee_profile.html');
 });
 
 app.get('/about', function (req, res) {
     res.render('about.html');
+});
+
+const userDB = new DataStore({filename:__dirname + '/gift-ee_users', autoload: true});
+app.get('/userlist', function (req, res) {
+	let userList = [];
+	
+	userDB.find({"firstName": /.../}, function(err, docs) {		// return all items in the database
+		if (err) {
+			console.log("something is wrong");
+		} else {
+			console.log("We found " + docs.length + " users");
+			for(let d of docs) {
+				console.log(`Name: ${d.firstName}`);
+				userList.push({"firstName": `${d.firstName}`})		// append database items to variable
+			}
+			res.render('user_list.html', {users: userList});		// had to change the tours.njk variable for the for-loop, compared to hw8
+		}
+	});
 });
 
 const PORT = process.env.PORT || 5000;
